710046291aa313d3dcc3927972471457f24fc50c
Karamjeet Singh <singhkaramjeetapple@gmail.com>
2023-12-30 14:17:45 -0800

    logout corrected

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
 
@@ -15,6 +19,6 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 
-router.route("/logout").post(verifyJWT, loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
